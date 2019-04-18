c880734e4a2cefb1d0962a32467213b32a840a82
Ruben Purdy <ruben.purdy@gmail.com>
2019-04-17 23:19:50 -0700

    Added hpc stuff for skiing

---
+++ skiing/extract.bash
@@ -0,0 +1,8 @@
+for i in `seq 1 28`;
+do
+  echo worker $i
+  # 358 trials per core with 28 cores will generate 10,024 trials total
+  singularity exec /extra/rubenpurdy/images/gym.simg xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python extract.py data/ --num_trials 358 &
+  sleep 1.0
+done
+wait
