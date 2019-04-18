18e1d443203d6a31785529f96454a5ca3d6db31e
Ruben Purdy <ruben.purdy@gmail.com>
2019-04-17 23:22:36 -0700

    updated job name

--- skiing/hpc/extract.sh
+++ skiing/hpc/extract.sh
@@ -3,7 +3,7 @@
 #PBS -l select=1:ncpus=28:mem=168gb:pcmem=6gb:ngpus=1
 
 ### Specify a name for the job
-#PBS -N wm_extract
+#PBS -N skiing_extract
 
 ### Specify the group name
 #PBS -W group_list=akoglu
