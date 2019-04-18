c3cb5ceaaecc9c20257e0b98b64c69160ce4adb1
Ruben Purdy <ruben.purdy@gmail.com>
2019-04-17 20:44:49 -0700

    Updated training vae to use new data

--- carracing/train_vae.py
+++ carracing/train_vae.py
@@ -1,47 +1,72 @@
 """Trains the vae on data created using `extract.py`."""
 import argparse
+import os
+
+import numpy as np
 
 from models.vae import get_vae
-from tensorflow.keras.utils import HDF5Matrix
 from tensorflow.keras.callbacks import ModelCheckpoint
 
 
-def scale(a):
-    return a / 255
+def create_dataset(data_dir, filelist, N=10000, M=1000):
+  data = np.zeros((M*N, 64, 64, 3), dtype=np.uint8)
+  idx = 0
+  for i in range(N):
+    filename = filelist[i]
+    raw_data = np.load(os.path.join(data_dir, filename))['obs'] / 255.0
+    l = len(raw_data)
+    if (idx+l) > (M*N):
+      data = data[0:idx]
+      print('premature break')
+      break
+    data[idx:idx+l] = raw_data
+    idx += l
+    if ((i+1) % 100 == 0):
+      print("loading file", i+1)
+  return data
 
 
 def main(args):
-    data_path = args.data_path
+    data_dir = args.data_dir
     epochs = args.epochs
     batch_size = args.batch_size
     checkpoint_path = args.checkpoint_path
     train_size = args.train_size
+    num_episodes = args.num_episodes
+    num_frames = args.num_frames
+
+    filelist = os.listdir(data_dir)
+    filelist.sort()
+    filelist = filelist[0:10000]
+
+    dataset = create_dataset(data_dir, filelist, N=num_episodes, M=num_frames)
 
     data_shape = (64, 64, 3)
     vae = get_vae(data_shape, 32)
 
-    x_train = HDF5Matrix(data_path, 'obs', end=train_size, normalizer=scale)
-
     checkpoint = ModelCheckpoint(checkpoint_path, monitor='train_loss')
 
     vae.compile(optimizer='adam')
-    vae.fit(x_train,
+    vae.fit(dataset,
             epochs=epochs,
             batch_size=batch_size,
-            shuffle='batch',
+            shuffle=True,
             callbacks=[checkpoint])
 
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Train the vae.')
-    parser.add_argument('--data_path', '-d', default='data/train.h5',
-                        help='The path to the training data.')
+    parser.add_argument('--data_dir', '-d', default='data/',
+                        help=('The path to the folder containing the training'
+                             'data.'))
     parser.add_argument('--epochs', '-e', type=int, default=10,
                         help='The number of epochs to train for.')
     parser.add_argument('--batch_size', '-b', type=int, default=128,
                         help='The batch size to use for training.')
     parser.add_argument('--checkpoint_path', default='checkpoints/vae.h5',
                         help='The path to save the checkpoint at.')
-    parser.add_argument('--train_size', type=int, default=10000000,
-                        help='The number of images to use for training.')
+    parser.add_argument('--num_episodes', type=int, default=10000,
+                        help='The number of episodes to use for training.')
+    parser.add_argument('--num_frames', type=int, default=1000,
+                        help='The number of frames per episode.')
     main(parser.parse_args())
