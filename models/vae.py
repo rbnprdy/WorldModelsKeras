2a8338b67d793fa5d309be1af05b115c29b2c26c
Ruben Purdy <ruben.purdy@gmail.com>
2019-04-23 07:50:27 -0700

    added vae parameters from github

--- models/vae.py
+++ models/vae.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.layers import Lambda, Input, Flatten, Dense, Conv2D, Conv2DTranspose, Reshape
 from tensorflow.keras.models import Model
 from tensorflow.keras import backend as K
+from tensorflow.keras.optimizers import Adam
 
 
 def sampling(args):
@@ -26,7 +27,9 @@ def get_vae(input_shape, latent_dim,
             deconv_filters=[128, 64, 32, 3],
             deconv_kernels=[5, 5, 6, 6],
             deconv_strides=[2, 2, 2, 2],
-            train=False):
+            train=False,
+            lr=0.0001,
+            kl_tolerance=0.5):
 
     inputs = Input(shape=input_shape, name='encoder_input')
     x = inputs
@@ -67,17 +70,26 @@ def get_vae(input_shape, latent_dim,
     vae = Model(inputs, outputs, name='vae')
 
     if train:
+        # def reconstruction_loss(y_true, y_pred):
+        #     return 10 * K.mean(
+        #         K.square(K.flatten(y_true) - K.flatten(y_pred)), axis = -1)
         def reconstruction_loss(y_true, y_pred):
-            return 10 * K.mean(
-                K.square(K.flatten(y_true) - K.flatten(y_pred)), axis = -1)
+            return K.mean(
+                K.sum(K.square(y_true - y_pred), axis=[1,2,3])
+            )
 
+        # def kl_loss(y_true, y_pred):
+        #     return - 0.5 * K.mean(
+        #         1 + K.log(sigma) - K.square(mu) - sigma, axis = -1)
         def kl_loss(y_true, y_pred):
-            return - 0.5 * K.mean(
-                1 + K.log(sigma) - K.square(mu) - sigma, axis = -1)
+            return K.maximum(
+                - 0.5 * K.sum(1 + K.log(sigma) - K.square(mu) - sigma, axis = -1),
+                kl_tolerance*latent_dim)
+
         
         def vae_loss(y_true, y_pred):
             return reconstruction_loss(y_true, y_pred) + kl_loss(y_true, y_pred)
         
-        vae.compile(optimizer='adam', loss=vae_loss, metrics=[reconstruction_loss, kl_loss])
+        vae.compile(optimizer=Adam(lr=lr), loss=vae_loss, metrics=[reconstruction_loss, kl_loss])
 
     return vae
