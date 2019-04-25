219ab2978d7d06f1a3e476cc85284b29061c4b31
Ruben Purdy <ruben.purdy@gmail.com>
2019-04-25 11:04:59 -0700

    uhh

--- environments/carracing/hpc/evolve_controller.sh
+++ environments/carracing/hpc/evolve_controller.sh
@@ -30,5 +30,5 @@ module load singularity
 cd /extra/rubenpurdy/WorldModelsKeras/environments/carracing
 
 date
-mpirun -np 28 singularity exec /extra/rubenpurdy/images/gym.simg xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python evolve_controller.py carracing --num_worker 27 --num_worker_trial 2 --num_episode 4
+mpirun -np 28 singularity exec /extra/rubenpurdy/images/gym_cpu.simg xvfb-run -a -s "-screen 0 1400x900x24 +extension RANDR" -- python evolve_controller.py carracing --num_worker 27 --num_worker_trial 2 --num_episode 4
 date
